c056d884e846117d912bbe025c1a1ebb9858fec6
Bruger <Bruger@W7-THINKPAD>
2016-10-04 10:58:29 +0200

    Crated main methods

--- Sandbox/BankAccount.cs
+++ Sandbox/BankAccount.cs
@@ -19,6 +19,39 @@ namespace Sandbox
             this.balance = balance;
             this.accountno = accountno;
         }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public int GetAccountno()
+        {
+            return accountno;
+        }
+
+        // Method for deposit
+        public void Deposit()
+        {
+
+        }
+
+        // Method for withdraw
+        public void Withdraw()
+        {
+
+        }
+
+        // Printout
+        public void PrintInformation()
+        {
+            Console.WriteLine("Account no {0} is owned by {1}", accountno, name);
+        }
     }
 
 
