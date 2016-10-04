7056bb053ee9b825c32f6c32b3aac5843c74fa8f
Bruger <Bruger@W7-THINKPAD>
2016-10-04 10:49:01 +0200

    Created a constructor

--- Sandbox/BankAccount.cs
+++ Sandbox/BankAccount.cs
@@ -7,5 +7,20 @@ namespace Sandbox
 {
     public class BankAccount
     {
+        // Instance fields
+        private string name;
+        private double balance;
+        private int accountno;
+
+        // Constructor
+        public BankAccount()
+        {
+            this.name = name;
+            this.balance = balance;
+            this.accountno = accountno;
+        }
     }
+
+
+
 }
